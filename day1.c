c08fee54f4fca73be03abc03b91ed7ceb2548975
Suhani Sethi <suhanisethi22.2007@gmail.com>
2025-09-25 10:08:15 +0530

    This C program takes two integer inputs from the user and performs basic arithmetic operations: addition, subtraction, multiplication, and division. It then displays the results of each operation.
    Header file inclusion:
    #include<stdio.h> is included to use standard input-output functions like printf and scanf.
    
    Variable declaration:
    
    a, b → store user inputs.
    
    sum, diff, product, div → store results of arithmetic operations.
    
    Input from user:
    
    The program asks the user to enter two integers (a and b) using scanf.
    
    Operations performed:
    
    Addition: sum = a + b
    
    Subtraction: diff = a - b
    
    Multiplication: product = a * b
    
    Division: div = a / b (integer division, so fractional part is discarded).
    
    Output:
    Each result is displayed using printf.

---
+++ day1.c
@@ -0,0 +1,14 @@
+// Q1 (User Inputs, Operations & Output) Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("enter a:");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+    sum = a + b;
+    printf("sum=%d",sum);
+}
+
