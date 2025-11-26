a421d9e4640b26ff916a8c164d9fc3d94a7ce305
Suhani Sethi <suhanisethi22.2007@gmail.com>
2025-11-26 22:46:19 +0530

    Create day27code2.c

---
+++ day27code2.c
@@ -0,0 +1,13 @@
+// Program to print a diamond pattern using stars (*)
+// The pattern will look like this:
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+#include <stdio.h>
+
+int main() {
+    did not understand
