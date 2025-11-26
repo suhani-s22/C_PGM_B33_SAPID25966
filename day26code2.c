a47c03a2dea0a8ea7a59063ab0db96448388c415
Suhani Sethi <suhanisethi22.2007@gmail.com>
2025-11-26 22:45:57 +0530

    Create day26code2.c

---
+++ day26code2.c
@@ -0,0 +1,45 @@
+// pattern printing using loops
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    // ----- Group 1: print 1 star -----
+    for(i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    printf("\n"); // blank line
+
+
+    // ----- Group 2: print 3 stars -----
+    for(i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+
+    printf("\n"); // blank line
+
+
+    // ----- Group 3: print 5 stars -----
+    for(i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+
+    printf("\n"); // blank line
+
+
+    // ----- Group 4: print 3 stars -----
+    for(i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+
+    printf("\n"); // blank line
+
+
+    // ----- Group 5: print 1 star -----
+    for(i = 1; i <= 1; i++) {
+        printf("*\n");
+    }
+
+    return 0
