c01d6349868dfcc8f6400ac1965f7896925c913b
Suhani Sethi <suhanisethi22.2007@gmail.com>
2025-11-26 22:46:51 +0530

    Create day29code2.c

---
+++ day29code2.c
@@ -0,0 +1,24 @@
+//Find the maximum and minimum element in an array.
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);  // Take size of array
+
+    int arr[n];  // Declare array of size n
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);  // Read each element
+        sum = sum + arr[i];   // Add element to sum
+    }
+
+    // Print the result
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
+
